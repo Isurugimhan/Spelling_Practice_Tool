bce461579e9a899ede98bf6af25b14909452cfae
Isurugimhan <114113834+Isurugimhan@users.noreply.github.com>
2025-04-25 15:11:10 +0530

    Update vite.config.ts

--- react-app/react-app/vite.config.ts
+++ react-app/react-app/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Spelling_Practice_Tool"
+  base: "/Spelling_Practice_Tool/"
 })
